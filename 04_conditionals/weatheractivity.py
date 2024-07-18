aca1ee6ffad86b05bcf709ebcbcd8fd66f84a496
Gaurav Kumar Singh <gs2600706@gmail.com>
2024-07-18 20:07:09 +0530

    program for weather condition

--- 04_conditionals/weatheractivity.py
+++ 04_conditionals/weatheractivity.py
@@ -1,10 +1,10 @@
 weather_status = input("Enter the current weather status :")
 
-if weather_status == "Sunny" or "sunny":
+if weather_status == "Sunny" or weather_status == "sunny":
     print("Go for a walk",end=".")
-elif weather_status == "Rainy" or "rainy":
+elif weather_status == "Rainy" or weather_status == "rainy":
     print("Read a book",end=".")
-elif weather_status == "Snowy" or "snowy":
+elif weather_status == "Snowy" or weather_status == "snowy":
     print("Build a snowman",end=".")
 else:
     print("Jo krna hai kr dimag mt kha",end=".")
