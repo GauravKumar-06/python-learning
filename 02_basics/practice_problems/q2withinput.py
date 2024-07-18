ad88ad5023509fbdd65e8b1b23d2c6f3aba431e4
Gaurav Kumar Singh <gs2600706@gmail.com>
2024-07-18 13:37:12 +0530

    program of calculating SI

---
+++ 02_basics/practice_problems/q2withinput.py
@@ -0,0 +1,7 @@
+principal = int(input("Enter the Principal amnt :"))
+rate = int(input("Enter the rate of interest :"))
+time = int(input("Enter the time :"))
+
+SI = (principal * rate * time)/100 
+
+print("Simple Interest is :",SI)
